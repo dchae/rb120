9c08aac3aa008b8cbcd836f0762576d1890d9a71
dchae <daniel.j.chae@gmail.com>
2023-04-16 20:06:17 -0700

    implemented play again

--- lesson_5/twentyone.rb
+++ lesson_5/twentyone.rb
@@ -6,6 +6,10 @@ class Player
     @cards = []
   end
 
+  def reset_cards
+    cards.clear
+  end
+
   def busted?
     total > 21
   end
@@ -41,7 +45,12 @@ end
 
 class Deck
   attr_reader :cards
+
   def initialize
+    reset_cards
+  end
+
+  def reset_cards
     @cards = 52.times.map { |ord| Card.new(ord) }.flatten
   end
 
@@ -52,15 +61,19 @@ class Deck
   def deal_to(player)
     player.add_to_hand(draw_card)
   end
+
+  private
+
+  attr_writer :cards
 end
 
 class Card
-  SUITS = %W[Clubs Diamonds Hearts Spades]
-  RANKS = %W[Ace Two Three Four Five Six Seven Eight Nine Ten Jack Queen King]
+  SUITS = %w[Clubs Diamonds Hearts Spades]
+  RANKS = %w[Ace Two Three Four Five Six Seven Eight Nine Ten Jack Queen King]
   attr_reader :rank, :suit
 
   def initialize(ord)
-    raise InputError if !(0..52).include?(ord)
+    raise(ArgumentError, "expected card number within range (0..52)") if !(0..52).include?(ord)
     @rank = ord % 13 + 1
     @suit = ord / 13 + 1
     @value = value + 1
@@ -85,27 +98,45 @@ class Game
   def initialize
     @deck = Deck.new
     @dealer = Dealer.new
-    @player = Player.new(get_player_name)
+    @player = Player.new(player_name)
   end
 
   def start
+    clear_screen
     greet_player
-    initial_deal
-    show_cards
-    player_turn
-    dealer_turn if !player.busted?
-    show_result
-    # binding.pry
+    loop do
+      initial_deal
+      show_cards
+      player_turn
+      dealer_turn if !player.busted?
+      show_result
+      break if !play_again?
+      reset_game
+      play_again_message
+    end
+    goodbye_message
   end
 
   private
+
+  def clear_screen
+    system "clear"
+  end
 
   def greet_player
     puts "Hello, #{player.name}. Welcome to Twenty-One!"
     puts
   end
 
-  def get_player_name
+  def goodbye_message
+    puts "Goodbye, #{player.name}. Thanks for playing Twenty-One!"
+  end
+
+  def play_again_message
+    puts "You chose to play again!"
+  end
+
+  def player_name
     name = nil
     loop do
       puts "What's your name?"
@@ -131,28 +162,29 @@ class Game
     puts "Value: #{player.total}\n\n"
   end
 
+  def player_choice
+    loop do
+      puts "Would you like to hit or stay? (h/s)"
+      choice = gets.chomp.downcase
+      return choice if choice =~ /^[hs]$/
+      puts "Not a valid choice."
+    end
+  end
+
+  def player_hit
+    deck.deal_to(player)
+    puts "You chose to hit.\nYou drew the #{player.cards.last}\n\n"
+  end
+
   def player_turn
     loop do
-      choice = nil
-      loop do
-        puts "Would you like to hit or stay? (h/s)"
-        choice = gets.chomp.downcase
-        break if choice =~ /[hs]{1}/
-        puts "Not a valid choice."
-      end
+      break if player_choice == "s"
 
-      if choice == "s"
-        puts "You chose to stay."
-        break
-      end
-
-      puts "You chose to hit."
-      deck.deal_to(player)
-      puts "You drew the #{player.cards.last}"
-      puts
-      break if player.busted?
+      player_hit
+      return if player.busted?
       show_cards
     end
+    puts "You chose to stay."
   end
 
   def dealer_turn
@@ -166,17 +198,41 @@ class Game
   def show_result
     puts
     show_cards(player_turn: false)
-    if player.busted?
-      puts "#{player.name} busted! #{dealer.name} wins."
-    elsif dealer.busted?
-      puts "#{dealer.name} busted. #{player.name} wins!"
-    elsif player.total > dealer.total
-      puts "#{player.name} wins!"
-    elsif player.total < dealer.total
-      puts "#{dealer.name} wins!"
-    else
+    show_winner
+  end
+
+  def show_winner
+    players = [player, dealer].sort_by(&:total)
+    winner = players.last
+    if players.any?(&:busted?)
+      puts "#{players.last.name} busted!"
+      winner = players.first
+    elsif player.total == dealer.total
       puts "It's a tie!"
+      return
     end
+    puts "#{winner.name} wins!"
+  end
+  
+  def validate_choice
+    # to implement
+  end
+
+  def play_again?
+    choice = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      choice = gets.chomp.downcase
+      break if choice =~ /^[yn]$/
+      puts "Not a valid choice"
+    end
+    choice == "y"
+  end
+
+  def reset_game
+    deck.reset_cards
+    [player, dealer].each { |x| x.reset_cards }
+    clear_screen
   end
 end
 
