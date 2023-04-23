ebcf2b724b259be2d93bc2c961f37345f8d85a6e
dchae <daniel.j.chae@gmail.com>
2023-04-22 18:35:49 -0700

    minilang cleaned up

--- exercises/07_medium-1/05.rb
+++ exercises/07_medium-1/05.rb
@@ -1,17 +1,18 @@
-class EmptyStackError < ArgumentError
+class MinilangProgramError < StandardError
+end
+class InvalidOperationError < MinilangProgramError
+  def initialize(op = nil, msg = "Invalid operation in program")
+    @op = op
+    super(msg + (op ? ": #{op}" : ""))
+  end
+end
+class EmptyStackError < MinilangProgramError
   def initialize(msg = "Required value not on stack (stack empty)")
     super(msg)
   end
 end
-
 class Minilang
-  MATH_OPERATORS = {
-    "ADD" => :+,
-    "SUB" => :-,
-    "MULT" => :*,
-    "DIV" => :/,
-    "MOD" => :%,
-  }
+  MATH_OPERATORS = { add: :+, sub: :-, mult: :*, div: :/, mod: :% }
 
   attr_reader :operations
 
@@ -19,35 +20,54 @@ class Minilang
     @operations = program.split
     @stack = []
     @register = 0
+    create_methods(MATH_OPERATORS)
   end
 
   def eval
-    operations.each do |op|
-      case op
-      when /\d/
-        self.register = op.to_i
-      when "PUSH"
-        stack << register
-      when "POP"
-        raise EmptyStackError if stack.empty?
-        self.register = stack.pop
-      when "PRINT"
-        puts register
-      else
-        if !MATH_OPERATORS.key?(op)
-          raise ArgumentError.new "Invalid operation in program: '#{op}'"
-        elsif stack.empty?
-          raise EmptyStackError
-        end
-
-        self.register = register.send(MATH_OPERATORS[op], stack.pop)
-      end
-    end
+    operations.each { |op| eval_op(op) }
+  rescue MinilangProgramError => e
+    puts e.message
   end
 
   private
 
   attr_accessor :stack, :register
+
+  def eval_op(op)
+    if op =~ /\d/
+      self.register = op.to_i
+    elsif self.class.private_method_defined?((op_sym = op.downcase.to_sym))
+      self.send(op_sym)
+    else
+      raise InvalidOperationError, op
+    end
+  end
+
+  def push
+    stack << register
+  end
+
+  def pop
+    raise EmptyStackError if stack.empty?
+    self.register = stack.pop
+  end
+
+  def print
+    puts register
+  end
+
+  def create_methods(hash)
+    # create a method for each name, operator pair in given hash
+    hash.each do |name, op|
+      self
+        .class
+        .define_method(name) do
+          self.register = self.register.send(op, self.pop)
+        end
+    end
+    # make these methods private
+    self.class.send :private, MATH_OPERATORS.keys
+  end
 end
 
 Minilang.new("PRINT").eval
@@ -65,11 +85,7 @@ Minilang.new("5 PUSH 10 PRINT POP PRINT").eval
 # 10
 # 5
 
-begin
 Minilang.new("5 PUSH POP POP PRINT").eval
-rescue => e
-  puts e.message
-end
 # Empty stack!
 
 Minilang.new("3 PUSH PUSH 7 DIV MULT PRINT ").eval
@@ -78,11 +94,7 @@ Minilang.new("3 PUSH PUSH 7 DIV MULT PRINT ").eval
 Minilang.new("4 PUSH PUSH 7 MOD MULT PRINT ").eval
 # 12
 
-begin
 Minilang.new("-3 PUSH 5 XSUB PRINT").eval
-rescue => e
-  puts e.message
-end
 # Invalid token: XSUB
 
 Minilang.new("-3 PUSH 5 SUB PRINT").eval
